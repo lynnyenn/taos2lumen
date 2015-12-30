9ecee2bbfc5077c559aed66cc5ed35333c346694
lynnyen <lynnyenn@gamil.com>
2015-12-30 17:06:31 +0800

    tset

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,4 @@
 <?php
-use  App\Http\Controllers;
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -9,81 +8,86 @@ use  App\Http\Controllers;
 | It is a breeze. Simply tell Lumen the URIs it should respond to
 | and give it the Closure to call when that URI is requested.
 |
-*/
+ */
 /*
 $app->get('/', function () use ($app) {
-    return $app->welcome();
+return $app->welcome();
 });
 
 //$app->get('/', 'HomeController@index');
 
 $app->group([
-    'prefix'     => Localization::setLocale(),
-    //'prefix'     => app('Localization')::setLocale(),
-    'middleware' => [
-        'localization-session-redirect',
-        'localization-redirect',
-         ],
-    ], function($app)
-    {
-        $app->get('/', function() {
-            return view('HomeController@index');
-        });
+'prefix'     => Localization::setLocale(),
+//'prefix'     => app('Localization')::setLocale(),
+'middleware' => [
+'localization-session-redirect',
+'localization-redirect',
+],
+], function($app)
+{
+$app->get('/', function() {
+return view('HomeController@index');
+});
 
-        $app->get('user/profile', function() {
-            // Uses Foo & Bar Middleware
-    });
+$app->get('user/profile', function() {
+// Uses Foo & Bar Middleware
+});
 });
 /*
 $app->get('/tw', function () use ($app) {
-    app('translator')->setLocale('tw');
+app('translator')->setLocale('tw');
 
-    //return $app->welcome();
+//return $app->welcome();
 });
 
 //$app->get('/{locale}', 'HomeController@index');
 $app->get('/', function () use ($app) {
-    return $app->welcome();
+return $app->welcome();
 });
-*/
+ */
 //$app->get('/', ['uses' => 'HomeController@index', 'lang' => 'name']);
 
 /*
 for key
-
-$app->get('/key', function() {
-    return str_random(32);
+ */
+$app->get('/key', function () {
+	return str_random(32);
 });
-*/
 
 //work
 //app('translator')->locale()
 //$locale = app('translator')->getLocale();
-if($_SERVER['REQUEST_URI'] == '/tw'){
-    app('translator')->setLocale('tw');
-    $locale = 'tw';
-}else{
-    app('translator')->setLocale('en');
-    $locale = '/';
+$urllang = explode('/', $_SERVER['REQUEST_URI'])[1];
+if ($urllang == 'tw') {
+	app('translator')->setLocale('tw');
+	$locate = 'tw/';
+} else {
+	app('translator')->setLocale('en');
+	$locate = '/';
 }
-$app->get($locale, [
-    'as' => 'home', 'uses' => 'HomeController@index'
+$app->get($locate, [
+	'as' => 'path', 'uses' => 'HomeController@index',
 ]);
-$app->get($locale.'news', [
-    'as' => 'news', 'uses' => 'NewsController@index'
-]);
+
+$app->get($locate . 'about', 'AboutController@index');
+$app->get($locate . 'news', 'NewsController@index');
+$app->get($locate . 'news', 'NewsController@index');
+$app->get($locate . 'news/{id}', 'NewsController@show');
+$app->get($locate . 'projects', 'ProjectsController@index');
+$app->get($locate . 'gallery', 'GalleryController@index');
+$app->get($locate . 'publications', 'PublicationsController@index');
 
 /*
 $app->group([
-    'prefix' => '',
-    //'middleware' => [ 'local_session','local_redirect'],
-    ], function($app)
-    {
-        $app->get('/', 'App\Http\Controllers\HomeController@index');
-        $app->get('/news', 'App\Http\Controllers\HomeController@index');
+'prefix' => '',
+//'middleware' => [ 'local_session','local_redirect'],
+], function($app)
+{
+$app->get('/', 'App\Http\Controllers\HomeController@index');
+$app->get('/news', 'App\Http\Controllers\HomeController@index');
 
-        $app->get('user/profile', function() {
-            // Uses Foo & Bar Middleware
-    });
+$app->get('user/profile', function() {
+// Uses Foo & Bar Middleware
 });
-*/
+});
+ */
